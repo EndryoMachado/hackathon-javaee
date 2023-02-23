7e31d76f42cf2480bfeb8ff63222c2b2c1b958d9
EndryoMachado <endryocostamachado@gmail.com>
2023-02-23 16:26:44 -0300

    implementando UsuarioDAO

---
+++ src/main/java/com/stefanini/dao/UsuarioDAO.java
@@ -0,0 +1,10 @@
+package com.stefanini.dao;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import com.stefanini.entity.UsuarioEntity;
+
+@ApplicationScoped
+public class UsuarioDAO  extends GenericDAO<UsuarioEntity, Long>{
+	
+}
